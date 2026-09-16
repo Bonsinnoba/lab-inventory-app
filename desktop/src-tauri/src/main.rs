d568ab3e55df407f7593bdbcfae55511ba2ae8bc
Balika Bonsinnoba <ivenbalika123@gmail.com>
2026-09-16 01:42:20 +0000

    feat: register offline movement cache command

--- desktop/src-tauri/src/main.rs
+++ desktop/src-tauri/src/main.rs
@@ -1,44 +1,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
 mod local_db;
 mod local_inventory;
 mod local_inventory_cache;
-
 fn main() {
-    let show = tauri::CustomMenuItem::new("show".to_string(), "Show");
-    let hide = tauri::CustomMenuItem::new("hide".to_string(), "Hide");
-    let quit = tauri::CustomMenuItem::new("quit".to_string(), "Quit");
-    let tray_menu = tauri::SystemTrayMenu::new().add_item(show).add_item(hide).add_item(quit);
-
-    tauri::Builder::default()
-        .setup(|app| {
-            local_db::initialize(&app.handle()).map_err(|err| Box::<dyn std::error::Error>::from(std::io::Error::new(std::io::ErrorKind::Other, err)))?;
-            local_inventory::initialize(&app.handle()).map_err(|err| Box::<dyn std::error::Error>::from(std::io::Error::new(std::io::ErrorKind::Other, err)))?;
-            Ok(())
-        })
-        .invoke_handler(tauri::generate_handler![
-            local_db::local_database_status,
-            local_db::save_local_snapshot_with_sync,
-            local_inventory::list_local_inventory,
-            local_inventory::upsert_local_inventory_item,
-            local_inventory::adjust_local_inventory,
-            local_inventory::list_local_inventory_movements,
-            local_inventory::save_local_inventory_movement,
-            local_inventory_cache::get_local_inventory_snapshot,
-            local_inventory_cache::cache_local_inventory_snapshot
-        ])
-        .system_tray(tauri::SystemTray::new().with_menu(tray_menu))
-        .on_system_tray_event(|app, event| match event {
-            tauri::SystemTrayEvent::LeftClick { .. } => { if let Some(window) = app.get_window("main") { let _ = window.show(); let _ = window.set_focus(); } }
-            tauri::SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
-                "quit" => std::process::exit(0),
-                "hide" => { if let Some(window) = app.get_window("main") { let _ = window.hide(); } }
-                "show" => { if let Some(window) = app.get_window("main") { let _ = window.show(); let _ = window.set_focus(); } }
-                _ => {}
-            },
-            _ => {}
-        })
-        .on_menu_event(|event| match event.menu_item_id() { _ => {} })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    let show=tauri::CustomMenuItem::new("show".to_string(),"Show"); let hide=tauri::CustomMenuItem::new("hide".to_string(),"Hide"); let quit=tauri::CustomMenuItem::new("quit".to_string(),"Quit"); let tray_menu=tauri::SystemTrayMenu::new().add_item(show).add_item(hide).add_item(quit);
+    tauri::Builder::default().setup(|app|{local_db::initialize(&app.handle()).map_err(|e|Box::<dyn std::error::Error>::from(std::io::Error::new(std::io::ErrorKind::Other,e)))?;local_inventory::initialize(&app.handle()).map_err(|e|Box::<dyn std::error::Error>::from(std::io::Error::new(std::io::ErrorKind::Other,e)))?;Ok(())}).invoke_handler(tauri::generate_handler![local_db::local_database_status,local_db::save_local_snapshot_with_sync,local_inventory::list_local_inventory,local_inventory::upsert_local_inventory_item,local_inventory::adjust_local_inventory,local_inventory::list_local_inventory_movements,local_inventory::cache_local_inventory_movements,local_inventory::save_local_inventory_movement,local_inventory_cache::get_local_inventory_snapshot,local_inventory_cache::cache_local_inventory_snapshot]).system_tray(tauri::SystemTray::new().with_menu(tray_menu)).on_system_tray_event(|app,event|match event{tauri::SystemTrayEvent::LeftClick{..}=>{if let Some(window)=app.get_window("main"){let _=window.show();let _=window.set_focus();}},tauri::SystemTrayEvent::MenuItemClick{id,..}=>match id.as_str(){"quit"=>std::process::exit(0),"hide"=>{if let Some(window)=app.get_window("main"){let _=window.hide();}},"show"=>{if let Some(window)=app.get_window("main"){let _=window.show();let _=window.set_focus();}},_=>{}},_=>{}}).on_menu_event(|event|match event.menu_item_id(){_=>{}}).run(tauri::generate_context!()).expect("error while running tauri application");
 }
